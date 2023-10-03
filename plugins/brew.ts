10a87e2d9ccd9d1ecdc9bc2ffd521bfb6891330a
Ando <iamando@tuta.io>
2023-10-03 16:40:16 +0300

    refactor: remove brew references on help command list

--- plugins/brew.ts
+++ plugins/brew.ts
@@ -20,26 +20,26 @@ class Brew implements Plugin {
       help: () => {
         console.log(`
         Example usage:
-            brew search TEXT|/REGEX/
-            brew info [FORMULA|CASK...]
-            brew install FORMULA|CASK...
-            brew update
-            brew upgrade [FORMULA|CASK...]
-            brew uninstall FORMULA|CASK...
-            brew list [FORMULA|CASK...]
+            search TEXT|/REGEX/
+            info [FORMULA|CASK...]
+            install FORMULA|CASK...
+            update
+            upgrade [FORMULA|CASK...]
+            uninstall FORMULA|CASK...
+            list [FORMULA|CASK...]
 
         Troubleshooting:
-            brew config
-            brew doctor
-            brew install --verbose --debug FORMULA|CASK
+            config
+            doctor
+            install --verbose --debug FORMULA|CASK
 
         Contributing:
-            brew create URL [--no-fetch]
-            brew edit [FORMULA|CASK...]
+            create URL [--no-fetch]
+            edit [FORMULA|CASK...]
 
         Further help:
-            brew commands
-            brew help [COMMAND]
+            commands
+            help [COMMAND]
         `);
         return Promise.resolve();
       },
