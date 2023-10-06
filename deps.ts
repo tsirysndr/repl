7787fdd0aa8bfbc95791f9612481fc5e3fbd82ef
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-06 13:19:46 +0000

    tui: add logo ASCII art

--- deps.ts
+++ deps.ts
@@ -1,2 +1,6 @@
 export { Input } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
-export { green, cyan } from "https://deno.land/std@0.203.0/fmt/colors.ts";
+export {
+  green,
+  cyan,
+  magenta,
+} from "https://deno.land/std@0.203.0/fmt/colors.ts";
