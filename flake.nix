170f21f4088c69c030cf79643910ab439f2201cd
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-19 11:35:20 +0000

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 
         packages.default = pkgs.deno2nix.mkExecutable {
           pname = "repl";
-          version = "0.6.2";
+          version = "0.7.0";
 
           src = ./.;
           lockfile = "./deno.lock";
